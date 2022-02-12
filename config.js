c30c10574f13fa433eb52a7a2a580104e4d45a64
Rieltod <99353680+Rieltod@users.noreply.github.com>
2022-02-12 19:47:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ global.APIKeys = { // APIKey nya disini
   'https://api.xteam.xyz': 'HIRO',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'nyyxz-bot',
-  'https://api.lolhuman.xyz': '886e0ade87d2da2cab7f1906',
+  'https://api.lolhuman.xyz': '2d118bcd18c4e779748a84b',
   'https://api.vhtear.com': 'sayahafiz',
   'https://fxc7-api.herokuapp.com': 'uN8rsK4g',
   'https://api.justaqul.xyz': '5kbUqdG00OXelFYx',
