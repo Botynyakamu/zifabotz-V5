dd4a562e741bfce5dd3542d1828c45aeb6c66739
Rieltod <99353680+Rieltod@users.noreply.github.com>
2022-02-13 18:12:44 +0800

    Update viagopay.js

--- plugins/viagopay.js
+++ plugins/viagopay.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Topup Rm10
+╠═ Untuk Pembayaran Via Topup gopay
 ╠➥ *chat owner*
 ║- Chat Owner Terlebebih
 ║ Dahulu
@@ -21,8 +21,8 @@ let info = `╠═〘 PEMBAYARAN 〙 ═
 ║   *FITUR LAINNYA*
 ║ 
 ╠═ ©2021 wabot-aq
-╠═ Scrip original by Lan Official
-╠═〘 LynXzy 〙 ═
+╠═ Scrip original by Riel Official
+╠═〘 RielXnzy 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
