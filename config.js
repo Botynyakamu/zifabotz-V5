80f0a72c0b82feb7e01ce75303a0b0e3ce70f626
Rieltod <99353680+Rieltod@users.noreply.github.com>
2022-02-14 17:59:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6282146218274', '60149431385','60189830350', '60149431385', '60149431385'] // Letakan nomor kamu disini
+global.owner = ['6282146218274', '6282146218274','6282146218274', '6282146218274', '6282146218274'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
