754b18c157aa0dffdbc8fd74b93237166a24831a
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 17:56:45 +0700

    Update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `Total Fitur dari Botz R-Txzy saat ini: ${totalf}`,m)
+conn.reply(m.chat, `Total Fitur dari ZIFABOTZ saat ini: ${totalf}`,m)
 }
 
 handler.help = ['totalfitur']
