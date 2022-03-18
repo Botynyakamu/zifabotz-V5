e09a218b2b1cdb5febf73ea680a2d1b129ae1939
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 17:24:00 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -2,26 +2,28 @@ let fetch = require('node-fetch')
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
     let kamisato = `
-┌〔 List Berlangganan 〕
-├ 2 Bulan
-├ 1 Bulan
-├ 1 Minggu
-├ Trial 1 Hari
-└────
-Silahkan klik pada "List Harga" untuk melihat list.
-
-Pembayaran:
-pulsa axis : 6283813417529
-pulsa telkomsel: 6282146218274
+╭═══ 〔 𝐋𝐈𝐒𝐓 𝐁𝐄𝐑𝐋𝐀𝐍𝐆𝐆𝐀𝐍𝐀𝐍 〕 ════
+├⬡ 𝟏 ᗷᑌᒪᗩᑎ
+├⬡ 4 ᗷᑌᒪᗩᑎ
+├⬡ 8 BULAN
+└═══════════════
+𝗦𝗶𝗹𝗮𝗵𝗸𝗮𝗻 𝗸𝗹𝗶𝗸 𝗽𝗮𝗱𝗮 "𝗟𝗶𝘀𝘁 𝗛𝗮𝗿𝗴𝗮" 𝘂𝗻𝘁𝘂𝗸 𝗺𝗲𝗹𝗶𝗵𝗮𝘁 𝗹𝗶𝘀𝘁.
+╭═══════════════════════
+║╭──❉ 〔 𝐕𝐈𝐀 𝐌𝐎𝐁𝐈𝐋𝐄 〕 ❉──────
+║│➸ *DANA* : 085828764046
+║│➸ *PULSA*: 085828764046
+║│➸ *GOPAY*: 085828764046
+║╰─────────
+▌│█║▌║▌║║▌║▌║█│▌▌│█║▌║▌║║▌║▌║█│
+╰═══════════════════════
 `.trim()
     const button = {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '2 Bulan', description: "Rp30.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rp25.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
-        {title: '1 Minggu', description: "Rp10.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
-        {title: 'Trial', description: "FREE\nBot gratis 1 Hari.", rowId:".join"},
+        {title: '4 Bulan', description: "Rp30.000\nSewa bot/premium selama 4 bulan.", rowId:".masuk"},
+        {title: '1 Bulan', description: "Rp15.000\nSewa bot/premium selama 1 bulan.", rowId:".masuk"},
+        {title: '8 bulan', description: "Rp40.000\nSewa bot/premium selama 8 bulan.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".snk"},
        ] }],
