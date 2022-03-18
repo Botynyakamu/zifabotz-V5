f47a6f622c3113e55d56de38c26516e51a0caf6a
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-19 06:06:57 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -21,9 +21,9 @@ let handler = async(m, { conn }) => {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '4 Bulan', description: "Rp30.000\nSewa bot/premium selama 4 bulan.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rp15.000\nSewa bot/premium selama 1 bulan.", rowId:".masuk"},
-        {title: '8 bulan', description: "Rp40.000\nSewa bot/premium selama 8 bulan.", rowId:".masuk"},
+        {title: '4 Bulan', description: "Rp30.000\nSewa bot selama 4 bulan.", rowId:".masuk"},
+        {title: '1 Bulan', description: "Rp15.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
+        {title: '8 bulan', description: "Rp40.000\nSewa bot selama 8 bulan.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".snk"},
        ] }],
