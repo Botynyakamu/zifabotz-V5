c557f30f563dd03019cce1b6c60adda6d7d9657a
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 17:32:00 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,8 +1,8 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply(users.map(v => 'wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+  m.reply(`🧑‍💻 *Owner ZIFABOTZ*\n\nhttps://wa.me/6285828764046\n\n🌟 *Bot Utama* \n\n• https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🪢JadiBot* \n\n` + users.map(v => '• https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
-handler.command = handler.help = ['listjadibot']
+handler.command = handler.help = ['listjadibot','ljb']
 handler.tags = ['jadibot']
 
 module.exports = handler
