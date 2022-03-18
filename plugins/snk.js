fb0d65da582955d86be70215b63c7dfe3bf3352b
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 17:27:11 +0700

    Update snk.js

--- plugins/snk.js
+++ plugins/snk.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn }) => await conn.send2Button(m.chat, `
 │ 9 & 10 = Block permanent
 ┬
 ╰──────────···
-${wm}
+Zifabotz
 `.trim(), 'Harap patuhin peraturan tersebut', '⋮☰ Menu', '.menu', 'Owner', '.owner', m)
 
 handler.help = ['rules']
