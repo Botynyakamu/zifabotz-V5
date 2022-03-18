c999019bb9dab87abf73564ab32e8136459148b1
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-19 04:02:56 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -1,48 +1,46 @@
+// RECODE ALYAAXZY
+
 let moment = require('moment-timezone')
-let fetch = require('node-fetch')
-let wm = global.botwm
-let logo = global.logo
+let fetch = require ('node-fetch')
 let handler = m => m
 
 handler.all = async function (m) {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let username = conn.getName(who)
 
     if (m.chat.endsWith('broadcast')) return
     if (m.fromMe) return
     if (m.isGroup) return
     if (db.data.settings.groupOnly) return
     let user = global.db.data.users[m.sender]
+    let name = conn.getName(m.sender)
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
-    await this.send3ButtonLoc(m.chat, logo, `
-*hai, ${ucapan()}*
-
-${user.banned ? 'kamu dibanned' : '───⬡ 〔 𝐁𝐎𝐓z 𝐂𝐇𝐀𝐓 〕 ⬡────
+    await conn.send2ButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), `
+*Hi ${name}, ${ucapan()}*
+───⬡ 〔 𝐁𝐎𝐓z 𝐂𝐇𝐀𝐓 〕 ⬡────
 ╭───────────────╮
         My name *ZIFABOTZ*
         Salah satu Bot diwhatsapp
 ╰───────────────╯
 Silahkan mematuhi Rules Bot
-Demi kenyamanan kita bersama.'}
-`.trim(), wm, user.register ? '⋮☰ Menu' : 'Verify', user.register ? '.menu' : `.daftar ${username}.13`, 'Rules', '.rules', 'Owner', '.owner', m)
+Demi kenyamanan kita bersama.
+`.trim(), watermark, 'Menu', '.?', 'Rules', '.rules')
     user.pc = new Date * 1
 }
 
 module.exports = handler
 function ucapan() {
     const time = moment.tz('Asia/Jakarta').format('HH')
-    res = "Selamat dinihari ☀️"
-    if (time >= 4) {
-        res = "Good Morning 🌄"
+    res = "Selamat dinihari"
+    if (time >= 4) { 
+        res = "Selamat pagi ya"
     }
     if (time > 10) {
-        res = "Good Afternoon ☀️"
+        res = "Selamat siang ya"
     }
     if (time >= 15) {
-        res = "Good Afternoon 🌇"
+        res = "Selamat sore ya"
     }
     if (time >= 18) {
-        res = "Good Night 🌙"
+        res = "Selamat malam ya"
     }
     return res
 }
