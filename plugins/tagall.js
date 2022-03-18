76c1934a558ebeaa147e9aef060a5cb016d78106
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 21:55:48 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,21 +1,28 @@
-let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
-  if (m.isGroup) {
-    if (!(isAdmin || isOwner)) {
-      global.dfail('admin', m, conn)
-      throw false
-    }
-  } else {
-    global.dfail('group', m, conn)
-    throw false
-  }
-  let users = participants.map(u => u.jid)
-  m.reply(`${text ? `${text}\n` : ''}┌─〔 Tag All 〕\n` + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
-    contextInfo: { mentionedJid: users }
-  })
-}
+let { MessageType } = require('@adiwajshing/baileys')
 
-handler.help = ['tagall']
-handler.tags = ['group', 'owner']
-handler.command = ['tagall']
+let handler = async (m, { conn, text, participants }) => {
+   r = text || 'gada pesannya'
+   l = conn.getName(m.sender)
+  let users = participants.map(u => u.jid)
+  conn.reply(m.chat, '║╭──❉ 〔 TAGALL 〕\n│\n║│➸*Nama:* ' + l +  '\n║│➸ *Pesan:* ' + r +'\n│\n' + users.map(v => '║│➸@' + v.replace(/@.+/, '')).join`\n`, m, {
+    contextInfo: { mentionedJid: users }
+ 
+  
+})
+}
+handler.command = /^(tagall|everyone)$/i
+handler.tags = ["group"]
+handler.help = ["tagall", "everyone"].map(a => a + ' <pesan>')
+handler.owner = false 
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = true
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
 
 module.exports = handler
