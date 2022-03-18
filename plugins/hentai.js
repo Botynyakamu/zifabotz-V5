b698bcb9f9086d156874b4bf344cbca2436d0a5f
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 16:37:03 +0700

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-   heum = await fetch(`https://api-alphabot.herokuapp.com/api/nsfw/hentai?apikey=Alphabot`)
+   heum = await fetch(`http://hadi-api.herokuapp.com/api/hentai`)
    if (!heum) throw json
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, 'Wangy wangy wangy', '© RielxBotz', 'NEXT', `${usedPrefix + command}`, m)
+   conn.sendButtonImg(m.chat, json, 'sange ye:v', '©️ zifaBotz', 'Lanjut oy', `${usedPrefix + command}`, m)
 }
 handler.help = ['hentai']
 handler.tags = ['hentai']
