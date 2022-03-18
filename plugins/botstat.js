aac999fe9c0efdf017f07b21a45c0570bc31ac33
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 17:30:13 +0700

    Update botstat.js

--- plugins/botstat.js
+++ plugins/botstat.js
@@ -8,28 +8,32 @@ let handler = async (m, { conn }) => {
     let uptime = clockString(_uptime)
 
     m.reply(`
-┌─〔 Status 〕
-├ Aktif selama ${uptime}
-├ Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
-├ *${groups.length}* Grup
-├ *${chats.length - groups.length}* Chat Pribadi
-├ *${Object.keys(global.db.data.users).length}* Pengguna
-├ *${totaljadibot.length}* Jadibot
-├ *${conn.blocklist.length}* Terblock
-├ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
-├ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
-└────
+╭═══════════════════════
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪STATUS ZIFABOTZ⳹ ❋ཻུ۪۪ 〕 ❉────── 
+║│➸ Aktif selama ${uptime}
+║│➸ Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
+║│➸ *${groups.length}* Grup
+║│➸ *${chats.length - groups.length}* Chat Pribadi
+║│➸ *${Object.keys(global.db.data.users).length}* Pengguna
+║│➸ *${totaljadibot.length}* Jadibot
+║│➸ *${conn.blocklist.length}* Terblock
+║│➸ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
+║│➸ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
+╰─────────❉
+_____•••••••••
 
-┌─〔 Pengaturan 〕
-├ ${anon ? '✅' : '❌'} *Anon Chat*
-├ ${anticall ? '✅' : '❌'} *Anti Call*
-├ ${antispam ? '✅' : '❌'} *Anti Spam*
-├ ${antitroli ? '✅' : '❌'} *Anti Troli*
-├ ${backup ? '✅' : '❌'} *Auto Backup DB*
-├ ${groupOnly ? '✅' : '❌'} *Mode Grup*
-├ ${jadibot ? '✅' : '❌'} *Jadi Bot*
-├ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
-└────
+╭═══════════════════════
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪PERATURAN ZIFABOTZ⳹ ❋ཻུ۪۪ 〕 ❉────── 
+║│➸ ${anon ? '✅' : '❌'} *Anon Chat*
+║│➸ ${anticall ? '✅' : '❌'} *Anti Call*
+║│➸ ${antispam ? '✅' : '❌'} *Anti Spam*
+║│➸ ${antitroli ? '✅' : '❌'} *Anti Troli*
+║│➸ ${backup ? '✅' : '❌'} *Auto Backup DB*
+║│➸ ${groupOnly ? '✅' : '❌'} *Mode Grup*
+║│➸ ${jadibot ? '✅' : '❌'} *Jadi Bot*
+║│➸ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
+╰─────────❉
+_____•••••••
     `.trim())
 }
 handler.help = ['botstatus']
