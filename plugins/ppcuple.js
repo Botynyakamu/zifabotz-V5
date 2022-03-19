bcae9f90cd978e0ef6732e4841d75a674395f7e7
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-19 18:54:36 +0700

    Update ppcuple.js

--- plugins/ppcuple.js
+++ plugins/ppcuple.js
@@ -1,7 +1,7 @@
 let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
-  let res = await fetch(global.API('LeysCoder', '/api/ppcouple', {}, 'apikey'))
+  let res = await fetch('https://api.xteam.xyz/randomimage/ppcouple?APIKEY=cristian9407')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
@@ -9,7 +9,8 @@ let handler = async (m, { conn }) => {
   await conn.sendFile(m.chat, json.result.female, '', 'cewe', m)
 }
 handler.help = ['ppcouple', 'ppcp']
-handler.tags = ['internet']
+handler.tags = ['internet', 'anime']
 handler.command = /^(pp(cp|couple))$/i
+handler.limit = true
 
 module.exports = handler
