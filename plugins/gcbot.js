99136a17a6aa3b94e7acc413bb32125824704a42
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-19 21:35:49 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -2,10 +2,13 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-✧─────[ *Group Official* ]─────✧
-ZIFABOTZ :
-https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv
-✧──────────···──────────✧
+╭═══════════════════════
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪⸙𝙕𝙞𝙛𝙖𝙗𝙤𝙩𝙯⳹ ❋ཻུ۪۪ 〕 ❉────── 
+║│➸ *_GROUP OFFC ZIFABKTZ_*
+║│➸  *https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv*
+╰─────────❉
+▌│█║▌║▌║║▌║▌║█│▌
+⳹ ❋ཻུ۪۪⸙𝙕𝙞𝙛𝙖𝙗𝙤𝙩𝙯⳹ ❋ཻུ۪۪⸙ by.rozi
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
 }
