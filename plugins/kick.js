e82cd317ffaa99e71a849abd406b4bb3596fb6d6
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-19 17:38:35 +0700

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -6,9 +6,16 @@ let handler = async (m, { conn, args }) => {
 handler.help = ['kick', '-'].map(v => v + ' @user')
 handler.tags = ['admin']
 handler.command = /^(kick|\-)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
 handler.group = true
+handler.private = false
+
 handler.admin = true
 handler.botAdmin = true
-handler.limit = true
-module.exports = handler
 
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
