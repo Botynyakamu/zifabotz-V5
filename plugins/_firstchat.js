05a6670584b9d39c7276add135ec858c113dbc04
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-19 23:18:31 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -15,13 +15,13 @@ handler.all = async function (m) {
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await conn.send2ButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), `
 *Hi ${name}, ${ucapan()}*
-───⬡ 〔 𝐁𝐎𝐓z 𝐂𝐇𝐀𝐓 〕 ⬡────
-╭───────────────╮
-        My name *ZIFABOTZ*
-        Salah satu Bot diwhatsapp
-╰───────────────╯
-Silahkan mematuhi Rules Bot
-Demi kenyamanan kita bersama.
+Saya adalah ZIFABOTZ salah satu bot WhatsApp.
+
+*Harap untuk tidak menelpon, meminta save, atau spam dalam penggunaan bot*
+Silahkan baca rules dan mematuhi rules demi kenyamanan kita bersama.
+Terimakasih
+
+Jika ada kendala silahkan hubungi owner.
 `.trim(), watermark, 'Menu', '.?', 'Rules', '.rules')
     user.pc = new Date * 1
 }
@@ -31,16 +31,16 @@ function ucapan() {
     const time = moment.tz('Asia/Jakarta').format('HH')
     res = "Selamat dinihari"
     if (time >= 4) { 
-        res = "Selamat pagi ya"
+        res = "Selamat pagi"
     }
     if (time > 10) {
-        res = "Selamat siang ya"
+        res = "Selamat siang"
     }
     if (time >= 15) {
-        res = "Selamat sore ya"
+        res = "Selamat sore"
     }
     if (time >= 18) {
-        res = "Selamat malam ya"
+        res = "Selamat malam"
     }
     return res
 }
