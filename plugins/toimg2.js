4db6f8843ffb49e0c63f279ef4fd011d23f61ae3
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-19 17:06:19 +0700

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -1,32 +1,18 @@
-let fs = require('fs')	
-const {
-    MessageType,
-    Mimetype
-} = require("@adiwajshing/baileys");
-const anu = {
-	key : {
-           participant : '0@s.whatsapp.net'
-                        },
-       message: {
-                    documentMessage: {
-                    title: 'Hmm.jpeg', 
-                    jpegThumbnail: fs.readFileSync('./src/R-Txzy.jpg')
-                          }
-                        }
-                      }
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-  if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
+  if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
   let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) throw `balas stiker dengan perintah *${usedPrefix + command}*`
+  if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
   let media = await m.quoted.download()
   let out = Buffer.alloc(0)
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', 'Nih kak', anu, false, { thumbnail: fs.readFileSync('./src/R-Txzy.png')})
+  await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m, false, {
+    thumbnail: Buffer.alloc(0)
+  })
 }
-handler.help = ['toimg']
+handler.help = ['toimg2 (reply)']
 handler.tags = ['sticker']
-handler.command = ['toimg']
+handler.command = ['toimg2']
 module.exports = handler
