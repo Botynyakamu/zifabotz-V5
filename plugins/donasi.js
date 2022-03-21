309a76f342bf5eb4781342670c18a019bfa3faa3
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-21 20:20:00 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,7 @@
+//Jangan Di Hapus Tapi kalo Di tambah Boleh
+
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(bank)).buffer(), `
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ╔═══════════════════
 ║ _*DONASI UNTUK ZIFABOTZ*_
 ╠═══════════════════
@@ -8,8 +10,8 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 ║│➸ *PULSA*: *_085828764046_*
 ║│➸ *GOPAY*: _*085828764046*_
 ║╰──────────────────
-╰═════```byzifabotz```
-`.trim(), 'JANGAN ASAL SALIN', 'BACK', '.menu')
+╰═════```by.zifabotz```
+`.trim(), watermark, 'MENU BOT', '.menu', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
