4ecd7d0d21c51952f7f687fcb3e658fd3cbc5eb1
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-23 21:49:36 +0700

    Update bokep.js

--- plugins/bokep.js
+++ plugins/bokep.js
@@ -1,14 +1,14 @@
-let fs = require('fs')
 let handler = async (m, { conn }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-	let stc = fs.readFileSync('./src/sukses.webp')
- conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Tobat banh', m)
+	const sukses = './src/sukses.webp'
+ conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih Jngn coli  ya', m)
+ conn.sendFile(m.chat, sukses, 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['bkp']
-handler.tags = ['bokep']
+handler.tags = ['hentai']
 
 handler.command = /^(bkp)$/i
-handler.premium = false
+handler.premium = true
 handler.register = true
-handler.limit = 30
+handler.limit = false
 module.exports = handler
