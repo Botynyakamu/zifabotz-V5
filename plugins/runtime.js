ead96b91940249f8c4e9bad0c6e636029aa36711
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-23 13:16:46 +0700

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -3,12 +3,12 @@ let _uptime = process.uptime() * 1000
 let uptime = clockString(_uptime)
 let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
 let runnya = `
-*───「 RUNTIME BOT🌻 」───*
+*───「 RUNTIME BOT▶️ 」───*
 
-🥀Time : ${time}
-🌸Runtime : ${uptime}
+⏳Time : ${time}
+📶Runtime : ${uptime}
 `
-conn.sendButton(m.chat, runnya, watermark, 'Menu', '.menu', m) 
+conn.sendButton(m.chat, runnya, watermark, 'makasih', '', m) 
 }
 handler.help = ['runtime']
 handler.tags = ['info']
