b9c24a1730c2ef69da24e055f9f472b658adaebe
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-23 22:48:19 +0700

    Update toimg.js

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -1,8 +1,21 @@
-// Coba dlu kalo error Fix sendiri aja
-
+let fs = require('fs')	
+const {
+    MessageType,
+    Mimetype
+} = require("@adiwajshing/baileys");
+const anu = {
+	key : {
+           participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    documentMessage: {
+                    title: 'Hmm.jpeg', 
+                    jpegThumbnail: fs.readFileSync('./src/RadBotZ.jpg')
+                          }
+                        }
+                      }
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-m.reply(wait)
   if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
   let mime = m.quoted.mimetype || ''
   if (!/webp/.test(mime)) throw `balas stiker dengan perintah *${usedPrefix + command}*`
@@ -11,7 +24,7 @@ m.reply(wait)
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', m, false, { thumbnail: out })
+  await conn.sendFile(m.chat, out, 'out.png', 'Sudah jadi...', anu, false, { thumbnail: fs.readFileSync('./src/RadBot.png')})
 }
 handler.help = ['toimg']
 handler.tags = ['sticker']
