d40aec6afbb1783e826646ddd0d7272052e0d35f
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-23 22:19:55 +0700

    Create biodata.js

---
+++ plugins/biodata.js
@@ -0,0 +1,41 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+  let ext= `
+╭═════════════════
+║╭──❉ *𝐁𝐈𝐎𝐃𝐀𝐓𝐀 𝐎𝐖𝐍𝐄𝐑* ❉───
+║│➸ *Nama* : rozi ( nama samaran ) 
+║│➸ *Umur* : 15
+║│➸ *Kelas* :  9 SMP
+║│➸ *Status* : butuh ayng AWOKAWOK
+║╰───────────────
+╰════════════════
+╭════════════════
+║╭──❉ *𝐒𝐎𝐒𝐈𝐀𝐋 𝐌𝐄𝐃𝐈𝐀* ❉───
+║│➸ *No.Owner* : wa.me/6285828764046
+║│➸ *No.Bot* : wa.me/6285828764046
+║│➸ *Chanel Youtube* : GAK ADA
+║│➸ *Gmail* : roziganteng559@gmail.com
+║│➸ *Github* : https://github.com/Botynyakamu 
+║╰────────────────
+╰═════════════════
+_Oke udah itu aja terimakasih_
+
+`.trim()
+conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'zifabotz', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
+
+}
+handler.help = ['infoowner', 'infocreator']
+handler.tags = ['info']
+handler.command = /^(infoowner|infocreator)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
