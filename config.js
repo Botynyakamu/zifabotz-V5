53960edb491c5646944ba6b3cb3820e50be101b8
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-24 16:24:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285828764046', '6285828764046','6285828764046', '6285828764046', '6285828764046'] // Letakan nomor kamu disini
+global.owner = ['6285828764046', '6285828764046','6281392779055', '6281392779055', '6281392779055'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
