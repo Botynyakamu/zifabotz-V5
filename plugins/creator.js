a5d3fc6da8b0a08b879ad9b106d8ba7d709ab6c6
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-24 16:30:05 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,26 +1,26 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-  let name = '@⳹ ❋ཻུ۪۪CREATOR ZIFABOTZ⳹ ❋ཻུ۪۪)'
+  let name = '@⳹ ❋ཻུ۪۪CREATOR UTAMA¹⳹ ❋ཻུ۪۪)'
   number = owner[0].replace(/[^0-9]/g, '')
   let njid = number + '@s.whatsapp.net'
   let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
 
-  let name2 = ' @⳹ ❋ཻུ۪۪CREATOR ZIFABOTZ⳹ ❋ཻུ۪۪)'
+  let name2 = ' @⳹ ❋ཻུ۪۪CREATOR UTAMA¹⳹ ❋ཻུ۪۪)'
   number2 = owner[1].replace(/[^0-9]/g, '')
   let njid2 = number2 + '@s.whatsapp.net'
   let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
 
-  let name3 = '@⳹ ❋ཻུ۪۪CREATOR ZIFABOTZ⳹ ❋ཻུ۪۪)'
+  let name3 = '@⳹ ❋ཻུ۪۪CREATOR KEDUA²⳹ ❋ཻུ۪۪)'
   number3 = owner[2].replace(/[^0-9]/g, '')
   let njid3 = number3 + '@s.whatsapp.net'
   let onW3 = await this.isOnWhatsApp(njid3) || { isBusiness: false }
 
-let name4 = '@⳹ ❋ཻུ۪۪CREATOR ZIFABOTZ⳹ ❋ཻུ۪۪)'
+let name4 = '@⳹ ❋ཻུ۪۪CREATOR KEDUA²⳹ ❋ཻུ۪۪)'
   number4 = owner[3].replace(/[^0-9]/g, '')
   let njid4 = number4 + '@s.whatsapp.net'
   let onW4 = await this.isOnWhatsApp(njid4) || { isBusiness: false }
-  this.send2Button(m.chat, `*Ngapain cari owner??*\n\nMau donasi/sewa bot ya:v.\nDi harapkan tidak menelpon/menyepam no owner.`, name4, 'Menu', '.menu', 'Profile', '.profile', m)
+  this.send2Button(m.chat, `*Ngapain cari owner??*\n\nMau donasi/sewa bot ya:v.\nDi harapkan tidak menelpon/menyepam no owner.`, name4, 'Menu', '.menu', 'Grup bot', '.gcbot', m)
   this.sendMessage(m.chat, {
     contacts: [{
       displayname: name, vcard: `
