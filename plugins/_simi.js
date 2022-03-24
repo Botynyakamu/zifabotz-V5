e8a1e4557043427520433b218e99a3f0d7a6c68a
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-24 12:03:34 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -7,11 +7,11 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned) {
         if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(API('xteam', '/simsimi', { kata: encodeURIComponent(m.text) }, ''))
-        if (!res.ok) return m.reply(`${res.status} ${res.statusText}`)
+        let res = await fetch(API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: 'id' }))
+        if (!res.ok) return m.reply(eror)
         let json = await res.json()
-        if (!json.status) return m.reply(require('util').format(json))
-        m.reply('*_' + json.jawaban + '_*')
+        if (json.success == "Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.") return m.reply('simi nya gk tau bang')
+        m.reply(json.success)
         return !0
     }
     return !0
