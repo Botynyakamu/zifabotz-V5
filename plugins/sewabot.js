5a58aad5df21263436138ce995e6b386ad5db1d7
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 17:38:05 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -21,9 +21,9 @@ let handler = async(m, { conn }) => {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '4 Bulan', description: "Rp30.000\nSewa bot selama 4 bulan.", rowId:".owner"},
-        {title: '1 Bulan', description: "Rp15.000\nSewa bot selama 1 bulan.", rowId:".owner"},
-        {title: '8 bulan', description: "Rp40.000\nSewa bot selama 8 bulan.", rowId:".owner"},
+        {title: '4 Bulan', description: "Rp30.000\nSewa bot selama 4 bulan.", rowId:".payment"},
+        {title: '1 Bulan', description: "Rp15.000\nSewa bot selama 1 bulan.", rowId:".payment"},
+        {title: '8 bulan', description: "Rp40.000\nSewa bot selama 8 bulan.", rowId:".payment"},
         {title: 'Owner', description: "Chat owner nya jika ingin sewa bot.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".rules"},
        ] }],
