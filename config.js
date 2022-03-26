c9dd12745600b33a67670736d21b859a31e455f6
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 15:58:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.APIKeys = { // APIKey nya disini
 global.packname = 'rozi'
 global.author = 'zifa'
 
-global.wait = '「 ⏱️ 」TUNGGU ZIFA lagi proses_-...'
+global.wait = '▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...'
 global.eror = '「❗」Server LAGI ERROR COK!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© zifabotz' //change the watermark 
