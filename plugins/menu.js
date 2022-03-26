6734f7abd4b3f7612574ee4c5fe79291f83e1a88
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 21:06:36 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -507,7 +507,7 @@ _____•••••••••
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), text.trim(), watermark, 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), text.trim(), watermark, 'Pemilik Bot', `${_p}owner`, 'skip', `.👍`, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
