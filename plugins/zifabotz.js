824ac55f51b1839f44dcb4bcef63f15916f74b8b
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 21:31:41 +0700

    Update zifabotz.js

--- plugins/zifabotz.js
+++ plugins/zifabotz.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let caption = `APA OI!!, MANGGIL” GAK JELAS`
 
-conn.sendButton( m.chat, caption, `>>zifabotz<<`, `>>zifabotz<<`, `>>zifabotz<<`, m)
+conn.sendButton( m.chat, caption, `>>zifabotz<<`, `>>zifabotz<<`, `.infoowner`, m)
 
        }
        
