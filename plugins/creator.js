d2976ed1265da42579ea4aa0583e3e75f066feb0
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-28 15:51:08 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,7 +20,7 @@ let name4 = '@⳹ ❋ཻུ۪۪CREATOR ZIFABOTZ⳹ ❋ཻུ۪۪)'
   number4 = owner[3].replace(/[^0-9]/g, '')
   let njid4 = number4 + '@s.whatsapp.net'
   let onW4 = await this.isOnWhatsApp(njid4) || { isBusiness: false }
-  this.send2Button(m.chat, `*Ngapain cari owner??*\n\nMau donasi/sewa DLL?.\nDi harapkan tidak menelpon/menyepam no owner.`, name4, 'Menu', '.menu', 'Group Bot', '.gcbot', m)
+  this.send2Button(m.chat, `*Ngapain cari owner??*\n\nMau donasi/sewa DLL?.\nDi harapkan tidak menelpon/menyepam no owner. Kalau owner satunya gak ngerspon SILAHAKN hubungi owner lainnya`, name4, 'Menu', '.menu', 'Group Bot', '.gcbot', m)
   this.sendMessage(m.chat, {
     contacts: [{
       displayname: name, vcard: `
