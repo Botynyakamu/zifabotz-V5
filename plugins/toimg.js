1678ff798023f9e26a591fc1b26806269461dc25
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-28 22:59:35 +0700

    Update toimg.js

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -1,17 +1,16 @@
-// Coba dlu kalo error Fix sendiri aja
-
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-m.reply(wait)
-  if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
-  let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) throw `balas stiker dengan perintah *${usedPrefix + command}*`
-  let media = await m.quoted.download()
-  let out = Buffer.alloc(0)
-  if (/webp/.test(mime)) {
-    out = await webp2png(media)
-  }
-  await conn.sendFile(m.chat, out, 'out.png', ©️zifabotz, false, { thumbnail: out })
+    if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
+    let media = await m.quoted.download()
+    let out = Buffer.alloc(0)
+    if (/webp/.test(mime)) {
+        out = await webp2png(media)
+    }
+    await conn.sendFile(m.chat, out, 'out.png', '*DAH NGAB*', m, false, {
+  thumbnail: Buffer.alloc(0)
+})
 }
 handler.help = ['toimg']
 handler.tags = ['sticker']
