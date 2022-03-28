8e80c8edf1203690279dd548a09f01d8ae6d9050
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-28 07:52:57 +0700

    Create anjj.js

---
+++ plugins/anjj.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn }) => {
+let caption = `anjir toxic🗿 awokawokaowk truskan bakat mu`
+
+conn.sendButton( m.chat, caption, `©️ zifabotz`, `>>anjay slibaw<<`, `.👍`, m)
+
+       }
+       
+handler.customPrefix = /^(anjing|anjj|anj|babi|bab1)/i
+handler.command = new RegExp
+module.exports = handler
