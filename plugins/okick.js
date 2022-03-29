2beb331ee96f295058a5ae7aa4d908479ea7ca5c
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-29 12:40:34 +0700

    Update okick.js

--- plugins/okick.js
+++ plugins/okick.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args }) => {
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
 handler.help = ['kick', '-'].map(v => 'o' + v + ' @user')
-handler.tags = ['owner']
+handler.tags = ['owner',admin]
 handler.command = /^(okick|o\-)$/i
 handler.owner = true
 handler.group = true
