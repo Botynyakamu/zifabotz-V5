666b7ccc5805ab457e15918376c1d52c6e5f8cec
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-29 14:35:26 +0700

    Update zifabotz2.js

--- plugins/zifabotz2.js
+++ plugins/zifabotz2.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
     "displayName": "ONWER AZIZ",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:AZIZ;;;\nFN:owner aziz\nitem1.TEL;waid=6288239938813:6288239938813\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:anidaanida59@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ AZIZ ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:AZIZ;;;\nFN:owner aziz\nitem1.TEL;waid=6288239938813:6288239938813\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:-\nitem2.X-ABLabel:💌 Email\nitem3.URL:-/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ AZIZ ]───────\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:WHATSAPP;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
