2faef25285f0ad9cc0adf4ca90e5022bd64cf1b8
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-29 13:34:08 +0700

    Create run.js

---
+++ plugins/run.js
@@ -0,0 +1,31 @@
+let fetch = require('node-fetch')
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = async(m, { conn }) => {
+    let kamisato = `
+╭═══ 〔 𝐋𝐈𝐒𝐓 𝐁𝐄𝐑𝐋𝐀𝐍𝐆𝐆𝐀𝐍𝐀𝐍 〕 ════
+├⬡ PERMANEN
+└═══════════════
+𝗦𝗶𝗹𝗮𝗵𝗸𝗮𝗻 𝗸𝗹𝗶𝗸 𝗽𝗮𝗱𝗮 "𝗟𝗶𝘀𝘁 𝗛𝗮𝗿𝗴𝗮" 𝘂𝗻𝘁𝘂𝗸 𝗺𝗲𝗹𝗶𝗵𝗮𝘁 𝗹𝗶𝘀𝘁.
+╭═══════════════════════
+║╭──❉ 〔 𝐕𝐈𝐀 𝐌𝐎𝐁𝐈𝐋𝐄 〕 ❉──────
+║│➸ *DANA*: 088233832771
+║╰─────────
+▌│█║▌║▌║║▌║▌║█│▌▌│█║▌║▌║║▌║▌║█│
+╰═══════════════════════
+`.trim()
+    const button = {
+        buttonText: 'List Harga',
+        description: kamisato,
+        sections:  [{title: "Silahkan di pilih", rows: [
+        {title: 'Owner', description: "Chat owner nya jika ingin sewa bot.", rowId:".owneraziz"},
+        {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".rules"},
+       ] }],
+        listType: 1
+       }
+    conn.sendMessage(m.chat, button, MessageType.listMessage, { quoted: m })
+}
+handler.tags = ['main']
+handler.command = /^(jasarun)$/i
+handler.help = ['jasarun']
+module.exports = handler
+//R-Txzy
