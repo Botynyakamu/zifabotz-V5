45411b6bfcc02e6143530582bcfbf30098fe8856
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-31 15:05:56 +0700

    Create tt.js

---
+++ plugins/tt.js
@@ -0,0 +1,19 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
+let botol = global.botwm
+let str = `
+╭═══════════════════════
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪TIKTOK⳹ ❋ཻུ۪۪ 〕 ❉────── 
+║│➸ *_TIKTOK OWNER_*
+║│➸  *https://github.com/Botynyakamu*
+╰─────────❉
+▌│█║▌║▌║║▌║▌║█│▌
+⳹ ❋ཻུ۪۪zifabotz⳹ ❋ཻུ۪۪⸙ by.rozi
+`.trim()
+conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
+}
+handler.help = ['tiktok']
+handler.tags = ['info']
+handler.command = /^tt$/i
+
+module.exports = handler
