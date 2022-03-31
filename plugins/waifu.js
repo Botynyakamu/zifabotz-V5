78d7c3159ca743ff98f696dad47d0673831120ed
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-31 19:55:18 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async(m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
   let res = await fetch('https://api.waifu.pics/sfw/waifu')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'nih kak', m)
+  conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Noh kak', watermark, 'berikutnya', `${usedPrefix + command}`, m,)
 }
 handler.help = ['waifu']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(waifu)$/i
-//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+
 module.exports = handler
