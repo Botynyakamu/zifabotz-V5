2aa93c7c8211bd9013525db94aa5232830e26e30
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-31 13:55:14 +0700

    Update R-Txzy.js

--- plugins/R-Txzy.js
+++ plugins/R-Txzy.js
@@ -1,10 +1,1 @@
-let handler = async (m, { conn }) => {
-let caption = `*BOT INI MENGGUNAKAN SC DARI https://github.com/Botynyakamu/Rzy.botz-M1*`
-
-conn.sendButton( m.chat, caption, `©️ zifabotz`, `thankyou`, `makasih`, m)
-
-       }
-       
-handler.customPrefix = /^(.sc|.script|.beli sc|.beli)/i
-handler.command = new RegExp
-module.exports = handler
+function _0x37c1(_0x39a845,_0xdb2117){const _0x4a116d=_0x4a11();return _0x37c1=function(_0x37c1ed,_0x37b03f){_0x37c1ed=_0x37c1ed-0x13e;let _0x425a3c=_0x4a116d[_0x37c1ed];return _0x425a3c;},_0x37c1(_0x39a845,_0xdb2117);}const _0x1c221f=_0x37c1;(function(_0x3f6e0f,_0x57f734){const _0x58358d=_0x37c1,_0x3b234b=_0x3f6e0f();while(!![]){try{const _0x423514=-parseInt(_0x58358d(0x14c))/0x1+parseInt(_0x58358d(0x148))/0x2+-parseInt(_0x58358d(0x13e))/0x3*(parseInt(_0x58358d(0x13f))/0x4)+-parseInt(_0x58358d(0x146))/0x5+parseInt(_0x58358d(0x141))/0x6+parseInt(_0x58358d(0x149))/0x7*(-parseInt(_0x58358d(0x143))/0x8)+parseInt(_0x58358d(0x142))/0x9;if(_0x423514===_0x57f734)break;else _0x3b234b['push'](_0x3b234b['shift']());}catch(_0x5f984e){_0x3b234b['push'](_0x3b234b['shift']());}}}(_0x4a11,0x45bff));let handler=async(_0x435b05,{conn:_0x5b5cb2})=>{const _0x6c6e2f=_0x37c1;let _0x2ebbaa=_0x6c6e2f(0x145);_0x5b5cb2[_0x6c6e2f(0x140)](_0x435b05['chat'],_0x2ebbaa,_0x6c6e2f(0x14d),_0x6c6e2f(0x14b),_0x6c6e2f(0x147),_0x435b05);};handler['customPrefix']=/^(.sc|.script|.beli sc|.beli)/i,handler[_0x1c221f(0x144)]=new RegExp(),module[_0x1c221f(0x14a)]=handler;function _0x4a11(){const _0x4697c2=['51yYBWIK','30116MMXeAZ','sendButton','996888nHYDAb','5934393NoUSZj','169976aCyZWl','command','*BOT\x20INI\x20MENGGUNAKAN\x20SC\x20DARI\x20https://github.com/Botynyakamu/Rzy.botz-M1*','2007935fNuaAk','makasih','1099060dfZWCh','91vmEOjH','exports','thankyou','283569mtVjxK','©️\x20zifabotz'];_0x4a11=function(){return _0x4697c2;};return _0x4a11();}
