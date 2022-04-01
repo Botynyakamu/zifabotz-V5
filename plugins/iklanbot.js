f3730f8739d23a70ea0709ebc6f9d187500735d6
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-04-02 00:09:20 +0700

    Update iklanbot.js

--- plugins/iklanbot.js
+++ plugins/iklanbot.js
@@ -13,7 +13,7 @@ let handler = async(m, { conn }) => {
         sections:  [{title: "STORE ZIFABOTZ", rows: [
         {title: 'Menu utama', description: "Kembali ke Menu Utama", rowId:".?"},
         {title: 'Sewa zifabotz', description: "Sewa bot dengan memasukkan bot ke grup kamu", rowId:".sewazifa"},
-        {title: 'Sewa satbotsss', description: "Sewa bot dengan memasukkan bot ke grup kamu", rowId:".sewabotsattt"},
+        {title: 'Sewa satbotsss', description: "Sewa bot dengan memasukkan bot ke grup kamu", rowId:".sewasat"},
         {title: 'Jasa run mursid', description: "jasa run bot", rowId:".jasarun"},
         {title: 'RULES', description: "rules zifabotz", rowId:".rules"},
        ] }],
