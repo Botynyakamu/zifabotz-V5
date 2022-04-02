6b5fceb6a0eb0ca56bd7802bb2d7e69419e1bb31
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-04-02 12:56:45 +0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -3,26 +3,38 @@ let handler = m => m
 let levelling = require('../lib/levelling')
 let fetch = require('node-fetch')
 handler.before = async function (m) {
-        let user = db.data.users[m.sender]
-        let users = Object.entries(db.data.users).map(([key, value]) => {
+        let user = global.db.data.users[m.sender]
+        let users = Object.entries(global.db.data.users).map(([key, value]) => {
                 return { ...value, jid: key }
         })
+        let pp = './src/avatar_contact.png'
         let who = m.sender
+        let role = global.db.data.users[m.sender].role
+        let exp = global.db.data.users[m.sender].exp
         let discriminator = who.substring(9, 13)
         let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
         let usersLevel = sortedLevel.map(enumGetKey)
         let { min, xp, max } = levelling.xpRange(user.level, global.multiplier)
-        if (!user.autolevelup) return !0
-        let before = user.level * 1
-        while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+        let username = conn.getName(who)
+        try {
+                pp = await this.getProfilePicture(who)
+        } catch (e) {
 
-        if (before !== user.level) {
-                let rank = await (await fetch(thumbfoto)).buffer()
-                        .then(async data => {
-                                await this.sendButtonLoc(m.chat, data, `@${who.split`@`[0]} _*Level Up!*_\n_${before}_ -> _${user.level}_`.trim(), watermark, 'Profile', ',my', m, { contextInfo: { mentionedJid: [who]}})
-                })
+        } finally {
+
+                if (!user.autolevelup) return !0
+                let before = user.level * 1
+                while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+
+                if (before !== user.level) {
+                        let rank = `https://telegra.ph/file/5b94da0fc1fafa4aa1be7.jpg`
+                        {
+                        	let tag = `@${m.sender.replace(/@.+/, '')}`
+                        	let mentionedJid = [m.sender]
+                                        await this.sendButtonLoc(m.chat, await (await fetch(image)).buffer(), `◪ *Name:* ${tag}\n├◆ *Role:* ${role}\n├◆ *Exp:* ${exp} xp\n╰◆ *Level:* ${before} ➠ ${user.level}`.trim(), watermark, 'STORE ZIFABOTZ', '.iklan', m, { contextInfo: { mentionedJid }})
+                                }
+                }
         }
-        
 }
 module.exports = handler
 
