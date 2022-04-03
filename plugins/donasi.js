ccb73743a9b4d84669217beb81609cbcc35769ed
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-04-03 16:40:37 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -15,7 +15,7 @@ let name = await conn.getName(m.sender)
 
 let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 
-  sumberImg = await (await fetch(fla + 'donasi zifabotz')).buffer()
+  sumberImg = await (await fetch(fla + 'zifabotz')).buffer()
   image = (await conn.prepareMessage('6285828764046@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
   res = await conn.prepareMessageFromContent(m.chat, {
     "productMessage": {
