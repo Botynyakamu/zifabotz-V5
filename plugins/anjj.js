83dda1270a17a5ad5e45e9e3f5ecf83288c3c1d4
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-04-05 21:29:55 +0700

    Update anjj.js

--- plugins/anjj.js
+++ plugins/anjj.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
 let caption = `anjir toxic🗿 awokawokaowk truskan bakat mu`
 
-conn.sendButton( m.chat, caption, `©️ zifabotz`, `>>anjay slibaw<<`, `.👍`, m)
+conn.sendButton( m.chat, caption, `©️ zifabotz`, `anjay slibaw`, `.👍`, m)
 
        }
        
-handler.customPrefix = /^(anjing|anjj|anj|babi|bab1|ngentod|sial|anjir|anjirt|kontol)/i
+handler.customPrefix = /^(anjing|anjj|anj|babi|bab1|ngentod|sial|anjir|anjirt|kontol|goblok|pantek|jahanam|ngentod|ngewe|bodoh)/i
 handler.command = new RegExp
 module.exports = handler
