7db8756c48ac7395eacdae0bd82622d25a421c06
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-04-05 15:39:45 +0700

    Update Zifa.js

--- plugins/Zifa.js
+++ plugins/Zifa.js
@@ -1,11 +1,11 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let fetch = require('node-fetch')
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Source+code'
-let info = `╭═[ *SCRIPT* ]═────···
+let info = `╭─────[ *SC BOT* ]─────✧
 ┴
-│ *Script :*
+│ *Script:*
 ┬
-╰──────────═┅═──────────
+╰──────────···
 [❗] Jangan diperjual belikan tanpa seizin owner
 `.trim()
 await conn.send2Button(m.chat, info, `${botol}`, '⋮☰ Back', '.menu', 'Owner', '.owner', m)
