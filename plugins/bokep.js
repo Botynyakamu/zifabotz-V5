f15876d56adced51a162c7271c945cbff27f728a
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-04-05 21:12:08 +0700

    Update bokep.js

--- plugins/bokep.js
+++ plugins/bokep.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 	const sukses = './src/sukses.webp'
- conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih Jngn coli  ya', m)
+ conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'jangan comli ye🗿', m)
  conn.sendFile(m.chat, sukses, 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['bkp']
