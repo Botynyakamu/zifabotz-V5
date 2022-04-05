e95757938f66339270e1e3d34aac0e50a33eca82
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-04-05 21:16:48 +0700

    Update testbot.js

--- plugins/testbot.js
+++ plugins/testbot.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let caption = `Gw tau lu cuman coba jaringan doang🗿`
 
-conn.sendButton( m.chat, caption, `©️ zifabotz`, `BISA ZIF!`, `.👍`, m)
+conn.sendButton( m.chat, caption, `©️ zifabotz`, `BISA ZIFA`, `.👍`, m)
 
        }
        
