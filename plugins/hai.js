cacbe52430253616d648ab4cb98dd3c067076da8
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-04-07 08:46:27 +0700

    Update hai.js

--- plugins/hai.js
+++ plugins/hai.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let caption = `*📮: HALLO KAK SAYA ZIFABOTZ,SILAHKAN KETIK .menu UNTUK MENGETAHUI FITUR BOT*`
+let caption = `*📮: HALLO KAK SAYA ZIFABOTZ,SILAHKAN KLIK BUTTON DI BAWAH UNTUK MENGETAHUI FITUR BOT*`
 
 conn.sendButton( m.chat, caption, `©️ zifabotz`, `Menu`, `.menu`, m)
 
