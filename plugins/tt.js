d8bd0bcf159587ad6f475b54b6fbc4091159b3b0
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-04-07 13:32:17 +0700

    Update tt.js

--- plugins/tt.js
+++ plugins/tt.js
@@ -1,10 +1,13 @@
-let handler = async (m, { conn }) => {
-let caption = `*Duh owner gw udah gak main TIKTOK lagi*`
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
+let botol = global.botwm
+let str = `
+*UDAH VAKUM MAIN TIKTOK!*
+`.trim()
+conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
+}
+handler.help = ['tiktok']
+handler.tags = ['info']
+handler.command = /^tt$/i
 
-conn.sendButton( m.chat, caption, `©️ zifabotz`, `Menu`, `.?`, m)
-
-       }
-       
-handler.customPrefix = /^(tt)/i
-handler.command = /^(tt)/i
 module.exports = handler
