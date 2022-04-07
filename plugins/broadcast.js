c012717512a11c0293bb9b83bd1cb27ef0999b70
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-04-07 19:55:14 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,23 +1,23 @@
+const fetch = require('node-fetch')
+
 let handler = async (m, { conn, text }) => {
-  let chats = conn.chats.all().filter(v => v.jid.endsWith('.net')).map(v => v.jid)
-  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
-  let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_\nestimasi selesai ${chats.length * 1.5} detik`, m)
-  for (let id of chats) {
-    await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '─────❏ *ʙʀᴏᴀᴅᴄᴀsᴛ* ❏─────\n\n' + teks + '\n\n' + watermark), true).catch(_ => _)
-  }
-  m.reply('_*Broadcast sukses dikirim*_')
+    let logo = global.logo
+    let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
+    conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chats_\nestimation complete ${chats.length * 1.5} seconds`, m)
+    for (let id of chats) {
+        await delay(1500)
+        await conn.send2ButtonLoc(id, logo,'*—「 Zifabotz Broadcast 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
+    }
+    m.reply('_*Broadcast berhasi di kirim*_')
 }
-handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
+handler.help = ['bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)$/i
+
 handler.owner = true
+
 module.exports = handler
 
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
-
 const delay = time => new Promise(res => setTimeout(res, time))
+
+let wm = global.botwm
