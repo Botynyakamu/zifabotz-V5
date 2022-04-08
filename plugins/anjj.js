259a90cd9cc2b46ab044acc8efc8de0fa20486a2
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-04-08 14:48:39 +0700

    Update anjj.js

--- plugins/anjj.js
+++ plugins/anjj.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let caption = `anjir toxic🗿 awokawokaowk truskan bakat mu`
+let caption = `*anjir toxic🗿 awokawokaowk truskan bakat mu*`
 
 conn.sendButton( m.chat, caption, `©️ zifabotz`, `anjay slibaw`, `.👍`, m)
 
