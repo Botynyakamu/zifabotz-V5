f930a26cf860c750ee4a8587b5240dd44d66a9ea
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-04-13 17:39:56 +0700

    Create scnoenc.js

---
+++ plugins/scnoenc.js
@@ -0,0 +1,44 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+	let fetch = require('node-fetch')
+    let fs = require('fs')
+const ftrol = {
+    key : {
+    remoteJid: 'status@broadcast',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2022,
+    status: 1,
+    surface : 1,
+    message: 'Script zifabotz', 
+    orderTitle: `▮Menu ▸`,
+    thumbnail: await (await fetch(fla + 'Script')).buffer(), //Gambarnye
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+    }
+let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Source+code'
+let info = `◪ 📮 *SCRIPT BOT NO ENC*
+*Beli lah ngab ya kalik free🗿*
+`.trim()
+await conn.send2Button(m.chat, info, `${botol}`, '⋮☰ MENU', '.menu', 'PROFILE', '.profile', ftrol)
+}
+handler.help = ['sc']
+handler.tags = ['tutor']
+handler.command = /^(scnoenc)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+
+module.exports = handler
+
+let botol = global.botwm
